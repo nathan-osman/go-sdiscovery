1f95361ea4f41f5e02d087aa49e4656278f79847
Nathan Osman <nathan@quickmediasolutions.com>
2015-05-26 22:50:52 -0700

    Added test for checkPeerTimeouts().

---
+++ service_test.go
@@ -0,0 +1,29 @@
+package sdiscovery
+
+import (
+	"testing"
+	"time"
+)
+
+func Test_checkPeerTimeouts(t *testing.T) {
+
+	// Create a new service with a peer timeout of 50 ms
+	svc := NewService(50*time.Millisecond, struct{}{})
+
+	// Manually inject the peer into the service
+	svc.Peers = append(svc.Peers, Peer{})
+
+	// Wait for the peer to be removed
+	timeout := time.After(100 * time.Millisecond)
+
+	select {
+	case <-svc.PeerRemoved:
+	case <-timeout:
+		t.Fatal("Timeout reached")
+	}
+
+	// Check the number of peers
+	if len(svc.Peers) != 0 {
+		t.Fatal("Peer not removed")
+	}
+}
