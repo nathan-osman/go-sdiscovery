db10378c2e1c531c2c9fa7b18238a33520f2e701
Nathan Osman <nathan@quickmediasolutions.com>
2015-05-29 15:47:17 -0700

    Reorganized the Service class.

--- service.go
+++ service.go
@@ -6,7 +6,8 @@ import (
 
 // ServiceConfig contains the parameters that control how the service behaves.
 // Note that it is important to keep the size of UserData to a minimum since
-// the entire struct is sent in each packet.
+// the entire struct is sent in each packet. This struct should not be modified
+// after being passed to NewService.
 type ServiceConfig struct {
 	PollInterval time.Duration // time between polling for network interfaces
 	PingInterval time.Duration // time between pings on the network
@@ -16,55 +17,99 @@ type ServiceConfig struct {
 	UserData     struct{}      // data sent with each packet to other peers
 }
 
-// Service provided on the local network
+// Service sends and receives packets on local network interfaces in order to
+// discover other peers providing the service and announce its presence.
 type Service struct {
-	PeerAdded   chan Peer
-	PeerRemoved chan Peer
-	Peers       []Peer
+	PeerAdded   chan Peer // indicates that a new peer was found
+	PeerRemoved chan Peer // indicates that an existing peer has timed out
 	stop        chan struct{}
-	config      *ServiceConfig
+	peers       map[string]Peer
+	config      ServiceConfig
 }
 
-// Create a new service with the specified peer timeout and user data.
-func NewService(config *ServiceConfig) *Service {
+// NewService creates a new Service instance with the specified configuration.
+func NewService(config ServiceConfig) *Service {
 
-	// Create the new service
 	s := &Service{
 		PeerAdded:   make(chan Peer),
 		PeerRemoved: make(chan Peer),
 		stop:        make(chan struct{}),
+		peers:       make(map[string]Peer),
 		config:      config,
 	}
 
-	// Create a ticker to schedule timeout checks
-	ticker := time.NewTicker(s.config.PeerTimeout)
-
-	// Spawn a new goroutine to check for peer timeouts
-	go func() {
-		for {
-			s.checkPeerTimeouts()
-			<-ticker.C
-		}
-	}()
+	// Spawn a new goroutine for:
+	// - sending and receiving packets
+	// - checking for expired peers
+	go s.run()
 
 	return s
 }
 
-// Check for peer timeouts
-func (s *Service) checkPeerTimeouts() {
+// Process pings and expire peers
+func (s *Service) run() {
 
-	// Create an empty slice pointing to the old array and filter the
-	// peers based on whether they have expired or not
-	peers := s.Peers[:0]
-	for _, peer := range s.Peers {
+	// Create a communicator for sending and receiving packets
+	communicator := newCommunicator(s.config.PollInterval, s.config.Port)
+	defer communicator.Stop()
 
-		// Update the peer and check if it has expired
-		peer.Update(s.config.PeerTimeout)
-		if peer.HasExpired() {
-			s.PeerRemoved <- peer
-		} else {
-			peers = append(peers, peer)
+	// Create a ticker for sending pings
+	pingTicker := time.NewTicker(s.config.PingInterval)
+	defer pingTicker.Stop()
+
+	// Create a ticker for timeout checks
+	timeoutTicker := time.NewTicker(s.config.PeerTimeout)
+	defer timeoutTicker.Stop()
+
+	for {
+		select {
+		case p := <-communicator.PacketReceived:
+			s.processPacket(p)
+		case <-pingTicker.C:
+			communicator.Send(s.config.ID, s.config.UserData)
+		case <-timeoutTicker.C:
+			s.processPeers()
+		case <-s.stop:
+			return
 		}
 	}
-	s.Peers = peers
+}
+
+// Process a packet received
+func (s *Service) processPacket(p packet) {
+
+	// Only process packets that did not originate from here
+	if p.ID != s.config.ID {
+
+		// If the peer does not exist, create a new one
+		_, exists := s.peers[p.ID]
+		if !exists {
+			s.peers[p.ID] = Peer{}
+		}
+
+		// Update the peer
+		s.peers[p.ID].Update(s.config.PeerTimeout)
+
+		// Write to the PeerAdded channel if the peer is new
+		if !exists {
+			s.PeerAdded <- s.peers[p.ID]
+		}
+	}
+}
+
+// Check for peer timeouts
+func (s *Service) processPeers() {
+	for id, peer := range s.peers {
+		if peer.HasExpired() {
+
+			// Write to the PeerRemoved channel and delete it
+			s.PeerRemoved <- peer
+			delete(s.peers, id)
+		}
+	}
+}
+
+// Stop the service
+func (s *Service) Stop() {
+	close(s.stop)
 }
