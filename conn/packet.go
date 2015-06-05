986a2838f1c5a430a10283992b7c6792a8b0dee8
Nathan Osman <nathan@quickmediasolutions.com>
2015-06-04 22:57:43 -0700

    Updated Packet class.

--- conn/packet.go
+++ conn/packet.go
@@ -5,30 +5,30 @@ import (
 	"net"
 )
 
-// Individual packet received from an interface
-type packet struct {
-	ip       net.IP
-	ID       string `json:"id"`
-	UserData []byte `json:"user_data"`
+// Packet represents an individual packet received from a network interface.
+type Packet struct {
+	IP       net.IP `json:"-"`         // IP address from which the packet was obtained
+	ID       string `json:"id"`        // ID of the peer that sent the packet
+	UserData []byte `json:"user_data"` // custom data provided by the peer
 }
 
-// Create a new packet from JSON data
-func newPacketFromJSON(ip net.IP, data []byte) (*packet, error) {
+// Create a new packet using the specified IP address and JSON data.
+func NewPacketFromJSON(ip net.IP, data []byte) (*Packet, error) {
 
-	// Create the packet with the provided IP address
-	pkt := &packet{
-		ip: ip,
+	// Create the packet with the provided IP address.
+	pkt := &Packet{
+		IP: ip,
 	}
 
-	// Attempt to unmarshal the JSON into the packet
-	if err := json.Unmarshal(data, &pkt); err != nil {
+	// Attempt to unmarshal the JSON into the packet.
+	if err := json.Unmarshal(data, pkt); err != nil {
 		return nil, err
 	}
 
 	return pkt, nil
 }
 
-// Convert a packet to JSON
-func (p *packet) toJSON() ([]byte, error) {
+// Convert the packet to JSON.
+func (p *Packet) ToJSON() ([]byte, error) {
 	return json.Marshal(p)
 }
