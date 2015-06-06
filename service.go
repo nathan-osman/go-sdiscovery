6616f49c5e74804184136f17c8c13b98a8679c44
Nathan Osman <nathan@quickmediasolutions.com>
2015-06-06 12:44:15 -0700

    Updated the documentation and tests for the Service class.

--- service.go
+++ service.go
@@ -7,10 +7,12 @@ import (
 	"github.com/nathan-osman/go-sdiscovery/peer"
 )
 
+type peerMap map[string]*peer.Peer
+
 // ServiceConfig contains the parameters that control how the service behaves.
 // Note that it is important to keep the size of UserData to a minimum since
-// the entire struct is sent in each packet. This struct should not be modified
-// after being passed to NewService.
+// the entire struct is sent in each packet. Any modifications to this struct
+// after passing it to NewService() will be ignored.
 type ServiceConfig struct {
 	PollInterval time.Duration // time between polling for network interfaces
 	PingInterval time.Duration // time between pings on the network
@@ -25,25 +27,23 @@ type ServiceConfig struct {
 type Service struct {
 	PeerAdded   chan string // indicates that a new peer was found
 	PeerRemoved chan string // indicates that an existing peer has timed out
-	stop        chan interface{}
-	peers       map[string]*peer.Peer
+	stopChan    chan interface{}
+	peers       peerMap
 	config      ServiceConfig
 }
 
-// NewService creates a new Service instance with the specified configuration.
-func NewService(config ServiceConfig) *Service {
+// New creates a new Service instance with the specified configuration.
+func New(config ServiceConfig) *Service {
 
 	s := &Service{
 		PeerAdded:   make(chan string),
 		PeerRemoved: make(chan string),
-		stop:        make(chan interface{}),
-		peers:       make(map[string]*peer.Peer),
+		stopChan:    make(chan interface{}),
+		peers:       make(peerMap),
 		config:      config,
 	}
 
-	// Spawn a new goroutine for:
-	// - sending and receiving packets
-	// - checking for expired peers
+	// Spawn a new goroutine for managing peers.
 	go s.run()
 
 	return s
@@ -61,67 +61,71 @@ func (s *Service) run() {
 	defer pingTicker.Stop()
 
 	// Create a ticker for timeout checks.
-	timeoutTicker := time.NewTicker(s.config.PeerTimeout)
-	defer timeoutTicker.Stop()
+	peerTicker := time.NewTicker(s.config.PeerTimeout)
+	defer peerTicker.Stop()
 
+	// Create the packet that will be sent to all peers.
 	pkt := &conn.Packet{
 		ID:       s.config.ID,
 		UserData: s.config.UserData,
 	}
 
+	// Continue processing events until explicitly stopped.
 	for {
-
 		select {
 		case p := <-communicator.PacketChan:
 			s.processPacket(p)
 		case <-pingTicker.C:
 			communicator.Send(pkt)
-		case <-timeoutTicker.C:
+		case <-peerTicker.C:
 			s.processPeers()
-		case <-s.stop:
+		case <-s.stopChan:
 			return
 		}
 	}
 }
 
-// Process a packet received.
+// Process a packet received from one of the connections.
 func (s *Service) processPacket(pkt *conn.Packet) {
 
-	// Only process packets that did not originate from here.
+	// Check the ID on the packet to ensure it does not match this peer.
 	if pkt.ID != s.config.ID {
 
-		// If the peer does not exist, create a new one.
+		// If the peer ID is not in the map, then create a new one.
 		_, exists := s.peers[pkt.ID]
 		if !exists {
 			s.peers[pkt.ID] = &peer.Peer{}
 		}
 
-		// Update the peer.
+		// Update the peer with the packet that was received.
 		s.peers[pkt.ID].Ping(pkt, time.Now())
 
-		// Write to the PeerAdded channel if the peer is new.
+		// If the peer didn't exist in the map prior to this packet, then send
+		// the peer ID over the PeerAdded channel.
 		if !exists {
 			s.PeerAdded <- pkt.ID
 		}
 	}
 }
 
-// Check for peer timeouts.
+// Check each of the peers in order to determine if any expired.
 func (s *Service) processPeers() {
 
+	// Avoid repeated calls to time.Now() by invoking it once here.
 	curTime := time.Now()
 
 	for id, peer := range s.peers {
 		if peer.IsExpired(s.config.PeerTimeout, curTime) {
 
-			// Write to the PeerRemoved channel and delete it.
+			// Send the peer ID over the PeerRemoved channel and remove it.
 			s.PeerRemoved <- id
 			delete(s.peers, id)
 		}
 	}
 }
 
-// Stop the service.
+// Stop the service. No more packets will be sent or received and all
+// connections will be closed.
 func (s *Service) Stop() {
-	close(s.stop)
+	close(s.stopChan)
 }
--- service_test.go
+++ service_test.go
@@ -1,7 +1,19 @@
 package sdiscovery
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
+// Ensure that the Service class can be instantiated and terminated.
 func Test_Service(t *testing.T) {
-	//...
+	s := New(ServiceConfig{
+		PollInterval: time.Second,
+		PingInterval: time.Second,
+		PeerTimeout:  time.Second,
+		Port:         8000,
+		ID:           "1234",
+		UserData:     nil,
+	})
+	defer s.Stop()
 }
